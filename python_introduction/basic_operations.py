664ad742e07ef72652fd8e14b2d2d954ee817e70
maxwel michira <maxwelmichira@gmail.com>
2025-11-09 02:31:31 +0300

    initail commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,20 @@
+# Objective: To preactice basic arthmetic operations in Python.
+
+# 1. Assign specific values to the thow variables
+number1 = 10
+number2 =n 5
+
+# 2. Perform the required arithmetic calculations
+# We use unique variable names to avoid confusion with built-in functions (Like sum)
+sum_result = number1 + number2
+difference = number1 + number2
+product = number1 * number2
+
+# 3. Print the results in the required human-readable format:
+# [operation] of [number1] and [number2] is [result]
+# The varialbes are embedded dierctly within curly braces {}
+
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")
+
