427d8666d390f717e69da6d86014f5e6c152218f
maxwel michira <maxwelmichira@gmail.com>
2025-11-27 22:43:08 +0300

    Creating a robust division calculator

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -7,6 +7,7 @@ def safe_divide(numerator, denominator):
 
     try:
         result = num / den
-        return f"Result: {result}"
+        return f"The result of the division is {result}"
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
+
