48d0c797410287fdbedfeb9692d2eb357f2c1338
maxwel michira <maxwelmichira@gmail.com>
2025-11-27 17:11:39 +0300

    Correct 2-decimal balance formatting

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,5 +12,4 @@ class BankAccount:
         return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
-
+        print(f"Current Balance: ${balance:.2f}")
