7037b8a034af3d77f86382ff45279efe4be2cd28
maxwel michira <maxwelmichira@gmail.com>
2025-12-05 16:13:25 +0300

    " updated  script to calculate area of Rectangle and Circle using polymorphism"

--- oop/oop/polymorphism_demo.py
+++ oop/oop/polymorphism_demo.py
@@ -1,13 +1,10 @@
 # polymorphism_demo.py
 import math
 
-# Base class
 class Shape:
     def area(self):
-        """Method to be overridden by subclasses"""
-        raise NotImplementedError("Subclasses must implement this method")
+        raise NotImplementedError("Subclasses must implement area method")
 
-# Derived class: Rectangle
 class Rectangle(Shape):
     def __init__(self, length, width):
         self.length = length
@@ -16,7 +13,6 @@ class Rectangle(Shape):
     def area(self):
         return self.length * self.width
 
-# Derived class: Circle
 class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
